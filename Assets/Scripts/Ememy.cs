b17050cadd921c20fc14f042063524c237e1e0d6
sang7j <83802491+sang7j@users.noreply.github.com>
2021-06-25 14:54:50 +0900

    sdf
    dsf

--- Assets/Scripts/Ememy.cs
+++ Assets/Scripts/Ememy.cs
@@ -13,6 +13,8 @@ public class Ememy : MonoBehaviour
     public float layDirection = 0.3f;
     public float layDistance = 0.5f;
 
+    
+
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -24,12 +26,14 @@ public class Ememy : MonoBehaviour
     void FixedUpdate()
     {
         //움직인 Move
-        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+        rigid.velocity = new Vector3(nextMove, rigid.velocity.y);
+        
 
         //Platform Check
         Vector2 frontVec = new Vector2(rigid.position.x + nextMove * layDirection, rigid.position.y);
-        Debug.DrawRay(frontVec, Vector2.down * layDistance, new Color(0, 1, 0)); // DrawRay : 에디터 상에서만 Ray를 그려주는 함수
+        Debug.DrawRay(frontVec, Vector2.down * layDistance, new Color(0, -1, 0)); // DrawRay : 에디터 상에서만 Ray를 그려주는 함수
         RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, layDistance, LayerMask.GetMask("Ground")); // RaycastHit : Ray에 닿은 오브젝트
+
 
         if (rayHit.collider == null)
         {
